73e154d0d5edb66df01dff6aa68f402e79ee155c
Ubuntu <ubuntu@ip-172-31-28-197.us-west-2.compute.internal>
2018-03-25 05:13:03 +0000

    First commit

---
+++ createUserGroovy.groovy
@@ -0,0 +1,10 @@
+
+import jenkins.model.*
+import hudson.security.*
+
+def instance = Jenkins.getInstance()
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount("MyUSERNAME","MyPASSWORD")
+instance.setSecurityRealm(hudsonRealm)
+instance.save() 
---
+++ removeScript.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo rm -rf /var/lib/jenkins/secrets/initialAdminPassword
+
